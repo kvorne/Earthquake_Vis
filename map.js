e69c9046b36e9f498ef6a8288a714d6bf53f8ece
kvorne <katarinavorne@gmail.com>
2022-11-21 23:33:35 -0600

    Moved map function to be in correct file.

--- map.js
+++ map.js
@@ -7,5 +7,23 @@ function printMap(mapData){
 }
 
 function getColor(mapDate, quakeDate){
+    
+}
 
+function drawWorld(error, countries) {
+    var width = 1000;
+    var height = 500;
+
+    var projection = d3.geoEquirectangular()
+        .fitExtent([[0,0], [width, height]], countries);
+    
+    var geoGenerator = d3.geoPath()
+        .projection(projection);
+
+    var paths = d3.select('svg')
+        .selectAll('path')
+        .data(countries.features)
+        .enter()
+        .append('path')
+        .attr('d', geoGenerator);
 }
