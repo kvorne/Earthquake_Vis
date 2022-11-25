9bcef7bbfce47e79b1cdb9a852902abb599cc3bc
kvorne <katarinavorne@gmail.com>
2022-11-25 17:29:33 -0600

    Created data frame to filter and print data by day

--- circles.js
+++ circles.js
@@ -1,33 +1,33 @@
-function getOpacity(mapDate, quakeDate){
-    numDaysAgo = mapDate-quakeDate;
-    return 100-((numDaysAgo-1)/30*100)
+function getOpacity(numDaysAgo){
+    return String(100-((numDaysAgo-1)/30*90))+"%";
 }
 
 function getColor(date){
     var value2color = d3.scaleSequential()
 				.domain([0, 366])
-				.interpolator(d3.interpolateRainbow);
+				.interpolator(d3.interpolateInferno);
     return value2color(date)
 }
 
 function getRadius(magnitude, scaleFactor){
-    return magnitude*scaleFactor;
+    return (Math.pow(magnitude*scaleFactor, 3)/50);
 }
 
 function drawCir(x,y,color, opacity, radius, isStroke, svg){
     //this will append a circle in the right spot
-    svg.append("circle")
+    var cir = svg.append("circle")
        .attr('r', radius)
        .attr('cx', x) 
        .attr('cy', y) 
        .attr('fill', color)
        .attr('opacity', opacity)
-       .attr('stroke-width', function (isStroke){
-            if (isStroke){ 
-                return "1px"
-            } else {
-                return "0px"
-            }
-        })
        .attr('stroke', "black");
+
+    if (isStroke) {
+        cir.attr('stroke-width', ".5px");
+    } else {
+        cir.attr('stroke-width', "0px");
+    }
+
+    return cir;
 }
