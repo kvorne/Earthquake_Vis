2b97eb25c615f1e47e16be7e4f58d9abdd58136e
kvorne <katarinavorne@gmail.com>
2022-12-15 17:24:54 -0600

    asdijfawoisdklfnasdfoinheatmap

--- animations.js
+++ animations.js
@@ -27,8 +27,28 @@ function fullYear(svg, year, earthquakes, projection){
 
         i++;
     }
-    // while(getYear(earthquakes[i].Date) < year){
-    //     console.log(getYear(earthquakes[i].Date));
-    //     i++;
-    // }
+}
+
+function allTime(svg, year, earthquakes, projection){
+    var circles = svg.selectAll("circle");
+    circles.remove();
+    const startYear = (element) => getYear(element.Date) == year;
+    start_index = earthquakes.findIndex(startYear, year);
+
+    for (let i = 0; i < earthquakes.length; i++){
+        curr = earthquakes[i];
+        let coordinates = projection([curr.Longitude, curr.Latitude]);
+
+        drawCir(
+            coordinates[0], 
+            coordinates[1], 
+            getColor(daysOutOf366(curr.Date)), 
+            "0%",
+            .1,
+            false, 
+            svg
+        );
+
+        i++;
+    }
 }
